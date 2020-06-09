8bbc9251539212278ea997142fb472a5b0d5c399
pkazimierowski <patryk.kazimierowski@gmail.com>
2020-06-09 22:26:16 +0200

    zaliczenie

--- zaliczenie.c
+++ zaliczenie.c
@@ -11,15 +11,14 @@ Przed zakończeniem programu wyświetl informację o numerze PID rodzica, dzieck
 
 
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
-//#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-//#include <fcntl.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <errno.h>
+
 
 int main(int argc, char *argv[])
 {
